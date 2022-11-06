16cadba6818621c46c0319faa9bddf87f2bdeb72
nogak <kdgure5@gmail.com>
2022-11-06 22:08:08 +0900

    [fin]Chapter6

---
+++ blog/lib/constants.js
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: 'CUBE',
+  siteDesc: 'アウトプットしていくサイト',
+  siteUrl: 'https://nogalab.tk',
+  siteLang: 'ja',
+  siteLocale: 'ja_JP',
+  siteType: 'website',
+  siteIcon: '/favicon.png',
+}
